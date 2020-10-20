317fbd89500506c108fd0ecff29c8ac9f1692068
hide1939 <hideto3970@gmail.com>
2020-10-20 21:25:16 +0900

    use文が間違っていてUesrモデルを使えていなかったため修正

--- kakeibo/tests/Http/Controllers/ExampleControllerTest.php
+++ kakeibo/tests/Http/Controllers/ExampleControllerTest.php
@@ -4,7 +4,7 @@ namespace Tests\Http\Controllers;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
-use App\User;
+use App\Models\User;
 
 class ExampleControllerTest extends TestCase
 {
