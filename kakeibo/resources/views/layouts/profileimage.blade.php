81b678dbd6112c7c23f9b3885c8c72acbefb1348
hide1939 <hideto3970@gmail.com>
2020-11-01 18:39:47 +0900

    プロフィール画像登録画面を仮実装

---
+++ kakeibo/resources/views/layouts/profileimage.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>プロフィール画像登録</title>
+</head>
+<body>
+    <p>テスト、これが表示されるはずですよ</p>
+    <script src="js/app.js"></script>
+</body>
+</html>
