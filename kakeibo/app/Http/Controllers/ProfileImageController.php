6e0946653027a01f3706c538e64eec71a5651696
hide1939 <hideto3970@gmail.com>
2020-11-03 14:00:31 +0900

    プロフィール画像を保存する処理とそのテストを実装

--- kakeibo/app/Http/Controllers/ProfileImageController.php
+++ kakeibo/app/Http/Controllers/ProfileImageController.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\UseCases\ProfileImageUseCase;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 class ProfileImageController extends Controller
 {
     public function edit()
     {
         return view('layouts.profileimage');
+    }
+
+    public function store(Request $request, ProfileImageUseCase $usecase)
+    {
+        $usecase->store($request->file('profile_image'), Auth::id());
+
+        return response(200);
     }
 }
--- kakeibo/tests/Http/Controllers/ProfileImageControllerTest.php
+++ kakeibo/tests/Http/Controllers/ProfileImageControllerTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Http\Controllers;
 
+use App\Models\User;
 use Tests\TestCase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 class ProfileImageControllerTest extends TestCase
 {
@@ -18,5 +21,41 @@ class ProfileImageControllerTest extends TestCase
     {
         $response = $this->get('/profile_image');
         $response->assertViewIs('layouts.profileimage');
+    }
+
+    /** @test */
+    public function storeでプロフィール画像の保存ができたら200が返る()
+    {
+        Storage::fake();
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->post('/profile_image', [
+            'profile_image' => UploadedFile::fake()->image('test.jpg')
+        ]);
+        $response->assertOk();
+    }
+
+    /** @test */
+    public function storeでデータベースにプロフィール画像のパスを保存できる()
+    {
+        Storage::fake();
+        $user = User::factory()->create();
+
+        $this->actingAs($user)->post('/profile_image', [
+            'profile_image' => UploadedFile::fake()->image('test.jpg')
+        ]);
+        $this->assertNotNull(User::find($user->id)->profile_image_path);
+    }
+
+    /** @test */
+    public function storeでストレージにプロフィール画像を保存できる()
+    {
+        Storage::fake();
+        $user = User::factory()->create();
+
+        $this->actingAs($user)->post('/profile_image',[
+            'profile_image' => UploadedFile::fake()->image('test.jpg')
+        ]);
+        Storage::disk()->assertExists('/profile_image/' . User::find($user->id)->profile_image_path);
     }
 }
