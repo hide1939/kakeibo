f55141d110fe50977b487f2c7dc7788598a72e44
hide1939 <hideto3970@gmail.com>
2020-11-04 22:15:34 +0900

    userファクトリにカラムとステート追加

--- kakeibo/database/factories/UserFactory.php
+++ kakeibo/database/factories/UserFactory.php
@@ -26,8 +26,18 @@ class UserFactory extends Factory
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
+            'profile_image_path' => null,
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
+    }
+
+    public function imagepath()
+    {
+        return $this->state(function () {
+            return [
+                'profile_image_path' => Str::random(20) . '.' . 'jpg'
+            ];
+        });
     }
 }
