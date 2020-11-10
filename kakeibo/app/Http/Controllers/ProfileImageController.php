b3f36ca56afff745fcdb5751547ae645d4d51d56
hide1939 <hideto3970@gmail.com>
2020-11-10 21:43:16 +0900

    認証の部分は先送りにしているため、とりあえず固定のユーザーidを指定

--- kakeibo/app/Http/Controllers/ProfileImageController.php
+++ kakeibo/app/Http/Controllers/ProfileImageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\UseCases\ProfileImageUseCase;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -17,19 +18,27 @@ class ProfileImageController extends Controller
 
     public function edit()
     {
-        return view('layouts.profileimage');
+        return view('layouts.profileimage', [
+            // TODO:ここも認証つけたら直す
+            'profile_image_path' => User::find(1)->profile_image_path
+            // 'profile_image_path' => User::find(Auth::id())->profile_image_path
+        ]);
     }
 
     public function store(Request $request)
     {
-        $this->usecase->store($request->file('profile_image'), Auth::id());
+        // TODO:一旦認証は先送り
+        // $this->usecase->store($request->file('profile_image'), Auth::id());
+        $this->usecase->store($request->file('profile_image'), 1);
 
-        return response(200);
+        return redirect('profile_image');
     }
 
     public function destroy() 
     {
-        $this->usecase->delete(Auth::id());
+        // TODO:一旦認証は先送り
+        // $this->usecase->delete(Auth::id());
+        $this->usecase->delete(1);
 
         return response(200);
     }
