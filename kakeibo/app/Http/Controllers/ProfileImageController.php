d53bd00bb592c2bd0b1fe33c6816f8140350bd31
hide1939 <hideto3970@gmail.com>
2020-11-13 06:24:39 +0900

    コントローラでモデルを直接操作したくないので、serviceから取得するようにした

--- kakeibo/app/Http/Controllers/ProfileImageController.php
+++ kakeibo/app/Http/Controllers/ProfileImageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Services\Query\UserService;
 use App\UseCases\ProfileImageUseCase;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,17 +12,20 @@ class ProfileImageController extends Controller
 {
     private $usecase;
 
-    public function __construct(ProfileImageUseCase $usecase)
+    private $service;
+
+    public function __construct(ProfileImageUseCase $usecase, UserService $service)
     {
         $this->usecase = $usecase;
+        $this->service = $service;
     }
 
     public function edit()
     {
         return view('layouts.profileimage', [
             // TODO:ここも認証つけたら直す+コントローラで直接Eloquentは触らない
-            'profile_image_path' => User::find(1)->profile_image_path
-            // 'profile_image_path' => User::find(Auth::id())->profile_image_path
+            'profile_image_path' => $this->service->getById(1)->profile_image_path
+            // 'profile_image_path' => $this->service->getById(Auth::id())->profile_image_path
         ]);
     }
 
