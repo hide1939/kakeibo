672c4c5df1630f7a33b2c60f24b9a9bff2dac375
hide1939 <hideto3970@gmail.com>
2020-11-14 16:01:09 +0900

    プロフィール画像登録時のロールバックの処理を追加

--- kakeibo/app/UseCases/ProfileImageUseCase.php
+++ kakeibo/app/UseCases/ProfileImageUseCase.php
@@ -3,7 +3,10 @@
 namespace App\UseCases;
 
 use App\Models\User;
+use Exception;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
@@ -11,18 +14,26 @@ class ProfileImageUseCase
 {
     public function store(UploadedFile $profile_image, $user_id)
     {
-        // DB
-        $user = User::find($user_id);
-        $previous_profile_image_path = $user->profile_image_path;
-        $profile_image_path = Str::random(20) . '.' . $profile_image->extension();
-        $user->profile_image_path = $profile_image_path;
-        $user->save();
+        DB::beginTransaction();
+        try {
+            // DB
+            $user = User::find($user_id);
+            $previous_profile_image_path = $user->profile_image_path;
+            $profile_image_path = Str::random(20) . '.' . $profile_image->extension();
+            $user->profile_image_path = $profile_image_path;
+            $user->save();
 
-        // storage
-        // 以前の画像をストレージから削除
-        Storage::delete('profile_image/' . $previous_profile_image_path);
-        // 新規画像をストレージに登録
-        Storage::putFileAs('profile_image', $profile_image, $profile_image_path);
+            // storage
+            // 以前の画像をストレージから削除
+            Storage::delete('profile_image/' . $previous_profile_image_path);
+            // 新規画像をストレージに登録
+            Storage::putFileAs('profile_image', $profile_image, $profile_image_path);
+        } catch (Exception $e) {
+            DB::rollBack();
+            Log::error('プロフィール画像登録処理中に例外が発生。DBをロールバックしました');
+            throw $e->getMessage();
+        }
+        DB::commit();
     }
 
     public function delete($user_id)
