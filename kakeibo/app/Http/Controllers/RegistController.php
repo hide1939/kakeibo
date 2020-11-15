a3e687e8634bcdd9a142a4d8e59aac21798891dc
hide1939 <hideto3970@gmail.com>
2020-11-15 17:16:51 +0900

    新規登録用のコントローラとそのテストを実装

---
+++ kakeibo/app/Http/Controllers/RegistController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\Command\UserService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class RegistController extends Controller
+{
+    private $service;
+
+    public function __construct(UserService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function create()
+    {
+        return view('layouts.regist');
+    }
+
+    public function store(Request $request)
+    {
+        $this->service->regist($request->all());
+
+        // name,emailのどちらか＋passwordで認証できる
+        if (!Auth::attempt($request->only('name', 'email', 'password'))) {
+            return redirect('/regist');
+        }
+
+        // TODO:サブスク機能・画面作ったら直す
+        return redirect('/profile_image');
+    }
+}
---
+++ kakeibo/tests/Http/Controllers/RegistControllerTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Tests\Http\Controllers;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
+
+class RegistControllerTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @test */
+    public function createでステータスコード200が返る()
+    {
+        $response = $this->get('/regist');
+        $response->assertOk();
+    }
+
+    /** @test */
+    public function createで新規登録画面が表示できる()
+    {
+        $response = $this->get('/regist');
+        $response->assertViewIs('layouts.regist');
+    }
+
+    /** @test */
+    public function storeでユーザーの新規登録ができる()
+    {
+        $this->post('/regist', [
+            'name' => 'test君',
+            'email' => 'test@example.com',
+            'password' => 'password@123',
+        ]);
+
+        $this->assertDatabaseHas('users', [
+            'name' => 'test君',
+            'email' => 'test@example.com',
+        ]);
+    }
+
+    /** @test */
+    public function storeで新規登録後は定期支出・収入画面にリダイレクトする()
+    {
+        $response = $this->post('/regist', [
+            'name' => 'test君',
+            'email' => 'test@example.com',
+            'password' => 'password@123',
+        ]);
+
+        // TODO:サブスク機能・画面作ったら直す
+        $response->assertRedirect('/profile_image');
+    }
+
+    /** @test */
+    public function storeで新規登録後はログイン状態になる()
+    {
+        $this->post('/regist', [
+            'name' => 'test君',
+            'email' => 'test@example.com',
+            'password' => 'password@123',
+        ]);
+
+        $this->assertTrue(Auth::check());
+    }
+
+    /** @test */
+    public function storeで認証状態にできなかった場合は新規登録画面にリダイレクトする()
+    {
+        Auth::shouldReceive('attempt')->andReturn(false);
+        $response = $this->post('/regist', [
+            'name' => 'test君',
+            'email' => 'test@example.com',
+            'password' => 'password@123',
+        ]);
+
+        $response->assertRedirect('/regist');
+    }
+}
