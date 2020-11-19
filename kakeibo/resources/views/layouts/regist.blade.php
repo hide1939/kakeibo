61a5b334ab6260569ef5d3265acd4443842a1cd1
hide1939 <hideto3970@gmail.com>
2020-11-19 22:43:09 +0900

    新規登録およびログイン用のフォームを実装

--- kakeibo/resources/views/layouts/regist.blade.php
+++ kakeibo/resources/views/layouts/regist.blade.php
@@ -6,5 +6,14 @@
 </head>
 <body>
     <div>新規登録だよ</div>
+    {{ Form::open(['url' => '/regist']) }}
+    {{ Form::label('name', '名前') }}
+    {{ Form::input('text', 'name')}}
+    {{ Form::label('email', 'email') }}
+    {{ Form::email('email') }}
+    {{ Form::label('password', 'パスワード') }}
+    {{ Form::password('password') }}
+    {{ Form::submit('送信') }}
+    {{ Form::close() }}
 </body>
 </html>
