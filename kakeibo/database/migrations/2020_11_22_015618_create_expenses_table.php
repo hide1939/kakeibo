2096faef71ce189df4b9d9e95eadb6228d27da23
hide1939 <hideto3970@gmail.com>
2020-11-22 11:19:03 +0900

    収支を登録するテーブルを定義した

---
+++ kakeibo/database/migrations/2020_11_22_015618_create_expenses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExpensesTable extends Migration
+{
+    /**
+     * 支出を登録するテーブル
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('item');
+            $table->integer('amount');
+            $table->boolean('is_regular');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expenses');
+    }
+}
