6fe3627c3aeca7d29dba6e92ecf363a9530794f5
hide1939 <hideto3970@gmail.com>
2020-11-23 16:19:56 +0900

    定期収支の項目と金額を表示するbladeを仮実装

--- kakeibo/resources/views/layouts/regular.blade.php
+++ kakeibo/resources/views/layouts/regular.blade.php
@@ -9,6 +9,20 @@
         <p>定期収支を登録する画面です</p>
         <p>定期収支の合計↓</p>
         <p>{{ $regular_total_amount }}</p>
+        <p>定期収入の項目と金額</p>
+        @foreach ($regular_expenses as $regular_expense)
+        <ul>
+            <li>{{ $regular_expense->item }}</li>
+            <li>{{ $regular_expense->amount }}</li>
+        </ul>
+        @endforeach
+        <p>定期支出の項目と金額</p>
+        @foreach ($regular_incomes as $regular_income)
+        <ul>
+            <li>{{ $regular_income->item }}</li>
+            <li>{{ $regular_income->amount }}</li>
+        </ul>
+        @endforeach
     </div>
 </body>
 </html>
