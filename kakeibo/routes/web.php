fb47953a646f5f31c652c0c3631766aa4b28918e
hide1939 <hideto3970@gmail.com>
2020-11-28 17:57:08 +0900

    画像を削除するためのルーティングを追加

--- kakeibo/routes/web.php
+++ kakeibo/routes/web.php
@@ -40,4 +40,5 @@ Route::group(['middleware' => ['auth']], function () {
     // 定期収支登録関連
     Route::get('/regular', [RegularController::class, 'edit']);
     Route::post('/regular', [RegularController::class, 'store']);
+    Route::delete('regular', [RegularController::class, 'destroy']);
 });
