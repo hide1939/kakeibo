47deb3d8a90e033f71bd10853f924d7f33720aa8
hide1939 <hideto3970@gmail.com>
2020-11-29 14:56:49 +0900

    home画面用のviewを作成

---
+++ kakeibo/resources/views/layouts/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>定期収支登録画面</title>
+</head>
+<body>
+    <div>
+        <a href="/regist">新規登録</a>
+        <a href="/login">ログイン</a>
+    </div>
+</body>
+</html>
