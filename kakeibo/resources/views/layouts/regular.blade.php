7a2e99305c5ecc09ba66579fec617a7750cd15b4
hide1939 <hideto3970@gmail.com>
2020-11-29 15:25:38 +0900

    viewのFormファサードの書き方が間違っていたので修正

--- kakeibo/resources/views/layouts/regular.blade.php
+++ kakeibo/resources/views/layouts/regular.blade.php
@@ -11,11 +11,11 @@
         </header>
         <div>
             <p>定期収支の合計</p>
-            <p>{{ $regular_total_amount }}</p>
+            <p>{{ $regular_total_amount }}円</p>
         </div>
         <div>
             <p>定期支出</p>
-            {{ Form::open(['url' => action([RegularController::class, 'store'], ['param' => 'e'])]) }}
+            {{ Form::open(['url' => '/regular?param=e']) }}
             {{ Form::label('item', '支出項目') }}
             {{ Form::input('text', 'item')}}
             {{ Form::label('amount', '支出金額') }}
@@ -24,7 +24,7 @@
             {{ Form::close() }}
             <p>-------------------</p>
             <p>定期収入</p>
-            {{ Form::open(['url' => action([RegularController::class, 'store'], ['param' => 'i'])]) }}
+            {{ Form::open(['url' => '/regular?param=i']) }}
             {{ Form::label('item', '収入項目') }}
             {{ Form::input('text', 'item')}}
             {{ Form::label('amount', '収入金額') }}
@@ -38,10 +38,7 @@
             <ul>
                 <li>{{ $regular_expense->item }}</li>
                 <li>{{ $regular_expense->amount }}</li>
-                {{ Form::open(['url' => action([RegularController::class, 'destroy'], [
-                    'param' => 'e',
-                    'id' => $regular_expense->id]
-                )]) }}
+                {{ Form::open(['method' => 'DELETE', 'url' => '/regular?param=e&id=$regular_expense->id']) }}
                 {{ Form::submit('削除') }}
                 {{ Form::close() }}
             </ul>
@@ -51,10 +48,7 @@
             <ul>
                 <li>{{ $regular_income->item }}</li>
                 <li>{{ $regular_income->amount }}</li>
-                {{ Form::open(['url' => action([RegularController::class, 'destroy'], [
-                    'param' => 'i',
-                    'id' => $regular_income->id]
-                )]) }}
+                {{ Form::open(['method' => 'DELETE', 'url' => '/regular?param=i&id=$regular_expense->id']) }}
                 {{ Form::submit('削除') }}
                 {{ Form::close() }}
             </ul>
