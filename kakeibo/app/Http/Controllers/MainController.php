213f259b44ab73da79bfebf4c64987514f7fe06d
hide1939 <hideto3970@gmail.com>
2020-12-03 22:24:31 +0900

    指定した年月を画面に表示する機能とそのテストを実装

---
+++ kakeibo/app/Http/Controllers/MainController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\Query\ExpenseService as QueryExpenseService;
+use App\Services\Query\IncomeService as QueryIncomeService;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class MainController extends Controller
+{
+    private $query_expense_service;
+    private $query_income_service;
+
+    public function __construct(
+        QueryExpenseService $query_expense_service,
+        QueryIncomeService $query_income_service
+    ) {
+        $this->query_expense_service = $query_expense_service;
+        $this->query_income_service = $query_income_service;
+    }
+
+    /**
+     * メイン画面を表示する
+     */
+    public function index(Request $request)
+    {
+        $year = is_null($request->query('y')) ? Carbon::now()->year : $request->query('y');
+        $month = is_null($request->query('m')) ? Carbon::now()->month : $request->query('m');
+
+        return view('layouts.main', [
+            'month_total_amount' => $this->query_income_service->getMonthTotalAmount(Auth::id(), $year, $month) 
+                - $this->query_expense_service->getMonthTotalAmount(Auth::id(), $year, $month)
+        ]);
+    }
+}
---
+++ kakeibo/tests/Http/Controllers/MainControllerTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Tests\Http\Controllers;
+
+use App\Http\Controllers\MainController;
+use App\Models\Expense;
+use App\Models\Income;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class MainControllerTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    private $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create();
+    }
+
+    /** @test */
+    public function indexはメイン画面を表示する()
+    {
+        $this->actingAs($this->user)->get('/main')
+            ->assertViewIs('layouts.main');
+    }
+
+    /** @test */
+    public function indexは指定した年月の収支を表示する()
+    {   
+        // 指定しない年月
+        Expense::factory()->create([
+            'user_id' => $this->user->id,
+            'amount' => 800,
+            'created_at' => '2018-10-4 20:00:00'
+        ]);
+        Income::factory()->create([
+            'user_id' => $this->user->id,
+            'amount' => 200,
+            'created_at' => '2016-08-09 14:08:34' 
+        ]);
+
+        // 指定する年月
+        Expense::factory()->create([
+            'user_id' => $this->user->id,
+            'amount' => 200,
+            'created_at' => '2020-10-4 20:00:00'
+        ]);
+        Income::factory()->create([
+            'user_id' => $this->user->id,
+            'amount' => 500,
+            'created_at' => '2020-10-09 14:08:34' 
+        ]);
+
+        $response = $this->actingAs($this->user)->get(action([MainController::class, 'index'], [
+            'y' => '2020',
+            'm' => '10'
+        ]));
+        $response->assertViewHas('month_total_amount', 300);
+    }
+
+    /** @test */
+    public function indexは年月を指定しない場合は今月の収支の合計値を表示する()
+    {
+        // 今月以外
+        Expense::factory()->create([
+            'user_id' => $this->user->id,
+            'amount' => 800,
+            'created_at' => '2018-10-4 20:00:00'
+        ]);
+        Income::factory()->create([
+            'user_id' => $this->user->id,
+            'amount' => 200,
+            'created_at' => '2016-08-09 14:08:34' 
+        ]);
+
+        // 今月
+        Expense::factory()->create([
+            'user_id' => $this->user->id,
+            'amount' => 200,
+            'created_at' => Carbon::now()
+        ]);
+        Income::factory()->create([
+            'user_id' => $this->user->id,
+            'amount' => 500,
+            'created_at' => Carbon::now()
+        ]);
+
+        // クエリパラメータで何も渡さない
+        $response = $this->actingAs($this->user)->get(action([MainController::class, 'index']));
+        $response->assertViewHas('month_total_amount', 300);
+    }
+}
