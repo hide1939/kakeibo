829d2a760cab24fc9b824d514426406ce933362c
hide1939 <hideto3970@gmail.com>
2020-12-13 17:50:48 +0900

    semantic uiを使ってデザイン仮実装

--- kakeibo/resources/views/layouts/base.blade.php
+++ kakeibo/resources/views/layouts/base.blade.php
@@ -4,12 +4,23 @@
     <meta charset="UTF-8">
     <title>@yield('title')</title>
     <script src="js/app.js"></script>
+    {{-- semantic ui --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js"></script>
 </head>
 <body>
-    <header></header>
-    <div>
+    <div class="ui inverted segment">
+        <div class="ui center aligned container">
+            <h2>kakeibo</h2>
+        </div>
+    </div>
+    <div class="ui container">
         @yield('content')     
     </div>
-    <footer></footer>
+    <div class="ui inverted segment">
+        <div class="ui center aligned container">
+            <h2>kakeibo</h2>
+        </div>
+    </div>
 </body>
 </html>
--- kakeibo/resources/views/layouts/home.blade.php
+++ kakeibo/resources/views/layouts/home.blade.php
@@ -5,4 +5,17 @@
 @section('content')
 <a href="/regist">新規登録</a>
 <a href="/login">ログイン</a>
+
+
+
+<div class="ui placeholder segment">
+  <div class="ui icon header">
+    <i class="search icon"></i>
+    We don't have any documents matching your query
+  </div>
+  <div class="inline">
+    <div class="ui primary button">Clear Query</div>
+    <div class="ui button">Add Document</div>
+  </div>
+</div>
 @endsection
