8032aa4d951d483939e2ab12d6e80a54e2918eec
hide1939 <hideto3970@gmail.com>
2020-12-14 22:10:59 +0900

    blade修正

--- kakeibo/resources/views/layouts/home.blade.php
+++ kakeibo/resources/views/layouts/home.blade.php
@@ -3,19 +3,18 @@
 @section('title', 'kakeibo')
 
 @section('content')
-<a href="/regist">新規登録</a>
-<a href="/login">ログイン</a>
-
-
-
 <div class="ui placeholder segment">
+    <!-- 画像入れる -->
   <div class="ui icon header">
-    <i class="search icon"></i>
-    We don't have any documents matching your query
+    シンプルな家計簿アプリです
   </div>
   <div class="inline">
-    <div class="ui primary button">Clear Query</div>
-    <div class="ui button">Add Document</div>
+    <button class="ui inverted orange button">
+        <a href="/regist">新規登録</a>
+    </button>
+    <button class="ui inverted blue button">
+        <a href="/login">ログイン</a>
+    </button>
   </div>
 </div>
 @endsection
