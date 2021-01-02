0d24c572b0eda37c316ded221b40ace29befeb47
hide1939 <hideto3970@gmail.com>
2021-01-02 16:14:21 +0900

    api_tokenを追加した

--- kakeibo/app/Services/Command/UserService.php
+++ kakeibo/app/Services/Command/UserService.php
@@ -4,12 +4,14 @@ namespace App\Services\Command;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserService
 {
     public function regist($regist_user_params)
     {
         $regist_user_params['password'] = Hash::make($regist_user_params['password']);
+        $regist_user_params['api_token'] = Str::random(60);
         User::create($regist_user_params);
     }
 } 
